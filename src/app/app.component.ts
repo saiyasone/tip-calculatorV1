e0f9362ade83b57bdf7c8d3c84096f94b48a2d6c
saiyasone <saiyasone595@gmail.com>
2023-09-07 12:56:40 +0700

    update all main files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-calculator';
+
+  tip = 5;
+  tipAmount = 0;
+  total = 0;
+
+  fBill = new FormControl('', [Validators.required]);
+  fPerson = new FormControl('', [Validators.required]);
+
+  setTip(data: number) {
+    if (this.fBill.invalid) {
+      return this.fBill.markAllAsTouched();
+    }
+
+    this.tip = data;
+  }
+
+  onCalculate() {
+    if (this.fBill.invalid) {
+      return this.fBill.markAllAsTouched();
+    }
+
+    let sum = 0;
+    let average = 0;
+    average = (+(this.fBill.value || 0) * this.tip) / 100;
+    this.tipAmount = average / +(this.fPerson.value || 0);
+    sum =
+      +(this.fBill.value || 0) / +(this.fPerson.value || 0) + this.tipAmount;
+    this.total = sum;
+  }
+
+  onReset() {
+    this.tip = 5;
+    this.tipAmount = 0;
+    this.total = 0;
+    this.fPerson.setValue('');
+    this.fBill.setValue('');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { isNumberDirective } from './number.directive';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, isNumberDirective],
+  imports: [BrowserModule, CommonModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
